3fa017c9c5c1fcdfc89bf50e9087f5df02e4af5a
RBH2004 <hassanrafidbin@gmail.com>
2025-08-22 00:54:57 +0600

    numpy

---
+++ array_func.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr=np.linspace(0,10,num=4)
+print(arr)
+#[ 0.          3.33333333  6.66666667 10.        ]
+#diagonal arraY
+
+
+arr=np.eye(3,5)
+
+print(arr)
+
+#[[1. 0. 0. 0. 0.]
+# [0. 1. 0. 0. 0.]
+# [0. 0. 1. 0. 0.]]
---
+++ arrays_dim.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x=[1,2,3,4]
+ar=np.array(x)
+
+print(type(ar))
+
+
+arr=np.array(x,ndmin=10)
+print(arr)
