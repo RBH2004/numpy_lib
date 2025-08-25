50ef503578091bc673c72ad289d383b4d8872096
RBH2004 <hassanrafidbin@gmail.com>
2025-08-25 20:59:30 +0600

    Update array_func.py

--- array_func.py
+++ array_func.py
@@ -44,3 +44,29 @@ array_range=np.arange(5)
 print(array_range)
 
 #[0 1 2 3 4] 
+
+
+random=np.random.rand(4)
+print(random)
+
+#any kind of number with range(0,1)
+#for example : [0.17823633 0.84123617 0.93194898 0.89321629]
+
+
+random_whole=np.random.randn(5)
+print(random_whole)
+
+#Both positive and negative numbers can be there with numbers close to 0
+#For example : [-0.20677452 -1.3369497   1.20079417 -1.04331284 -0.35277995]
+
+random_f=np.random.ranf(5)
+
+print(random_f)
+
+#[0.72061847 0.42650527 0.67011744 0.04146737 0.13604658]
+
+
+random_range_of_int=np.random.randint(0,80,5)
+print(random_range_of_int)
+#function(min,max,total)
+#[54 74 76 25 54]
