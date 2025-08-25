45a0ebd7cd40749540975b0a7c67e7cf7844b26f
RBH2004 <hassanrafidbin@gmail.com>
2025-08-25 20:25:52 +0600

    Update array_func.py

--- array_func.py
+++ array_func.py
@@ -9,7 +9,38 @@ print(arr)
 arr=np.eye(3,5)
 
 print(arr)
+print(arr.ndim)
+
+
 
 #[[1. 0. 0. 0. 0.]
 # [0. 1. 0. 0. 0.]
 # [0. 0. 1. 0. 0.]]
+
+
+
+zeros=np.zeros(5)
+print(zeros)
+#[0. 0. 0. 0. 0.]
+ones=np.ones(5)
+print(ones)
+#[1. 1. 1. 1. 1.]
+mat_zeros=np.zeros((3,5))
+
+print(mat_zeros)
+
+
+
+#[[0. 0. 0. 0. 0.]
+# [0. 0. 0. 0. 0.]
+# [0. 0. 0. 0. 0.]]
+
+empty=np.empty(5)
+print(empty)
+
+#[1. 1. 1. 1. 1.]
+
+array_range=np.arange(5)
+print(array_range)
+
+#[0 1 2 3 4]
