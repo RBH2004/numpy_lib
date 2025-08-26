deec867142c0dfbfb9066305b82b33e55830f4d9
RBH2004 <hassanrafidbin@gmail.com>
2025-08-26 12:20:17 +0600

    Create arithmatic_op.py

---
+++ arithmatic_op.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+arr=np.array([1,2,3,4])
+new_arr=arr/2
+
+print(new_arr)
+
+#[0.5 1.  1.5 2. ]
+
+arr2=np.array([4,5,6,7])
+arr2=arr+arr2
+print(arr2)
+#shape must be similar
+#[ 5  7  9 11]
+
+
+new_add=np.add(arr,arr2)#similar subrtact,multiply,divide,mod,power,reciprocal
+#reciprocal is 1/x if x is given as a number
+print(new_add)
+
+#[ 6  9 12 15]
+
+
