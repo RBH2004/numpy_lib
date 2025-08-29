3c9fc096163ee4451e908c56f4fdca8ce628bac1
RBH2004 <hassanrafidbin@gmail.com>
2025-08-29 12:15:17 +0600

    Update numpy_find.py

--- numpy_find.py
+++ numpy_find.py
@@ -1,7 +1,15 @@
 import numpy as np
 
-arr=np.array([1,2,3,4,5])
+arr=np.array([1,2,3,4,5,6])
 
 find=np.where((arr/2)==2)
 print(find)
 #(array([3], dtype=int64),)
+
+
+idx=np.searchsorted(arr,5,side="right")
+print(idx)
+print(arr)
+
+#5
+#[1 2 3 4 5 6]
